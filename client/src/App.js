f2dc2acffc53cc75e394209f9c0d112a2c661a0b
Roger Oporta <roportaperez@gmail.com>
2022-08-12 17:34:38 -0600

    Example of useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -2,53 +2,18 @@ import React, { useEffect, useState } from 'react'
 
 function App() {
 
-  // const [backendData, setBackendData] = useState([{}])
+  const [counter, setCounter] = useState(0);
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   fetch("/api").then(
-
-  //     response => response.json()
-
-  //   ).then(
-  //     data => {
-  //       setBackendData(data)
-  //     }
-  //   )
-
-  // }, [])
-
-  // return (
-
-  //   <div>
-
-  //     {(typeof backendData.pets === 'undefined') ? (
-  //       <p>Loading ...</p>
-  //     ) : (
-  //       backendData.pets.map((pet, i) => (
-  //         <p key={i}>{pet}</p>
-  //       ))
-  //     )}
-
-  //   </div>
-  // )
-
-  // useEffect(() => {
-
-
-
-  // }, [])
-
-  const [flag, setFlag] = useState(true);
+  });
 
   return (
     <div className='App'>
-      {flag ? <p>TRUE</p> : <p>FALSE</p>}
-      <button onClick={ () => flag ? setFlag(false) : setFlag(true) } >Click ME!!</button>
-
+      <p>{counter}</p>
+      <button onClick={() => { setCounter( counter + 1 )}}> Press me! </button>
     </div>
-  );
-
+  )
 }
 
 export default App
