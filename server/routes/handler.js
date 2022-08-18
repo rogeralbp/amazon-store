b90d54b35c19148cf62ae792ecec9fd81999323c
Roger Oporta <roportaperez@gmail.com>
2022-08-18 10:44:38 -0600

    Better endpoints

--- server/routes/handler.js
+++ server/routes/handler.js
@@ -10,16 +10,51 @@ router.get('/apps', async (req, res) => {
         if (err) throw err;
 
         try {
-            
-            const query = `SELECT * FROM world.city`;
+            const query = `SELECT * FROM world.city LIMIT 0,10`;
             conn.query(query, (err, result) => {
                 conn.release();
                 if (err) throw err;
-                res.send(JSON.stringify(result));
+                res.send({ status: "OK", data: result });
             });
-        } catch (err){
+        } catch (err) {
 
-            console.log(err);
+            res
+                .status(err?.status || 500)
+                .send({ status: "FAILED", data: { error: err?.message || err } });
+            res.end();
+        }
+
+    });
+});
+
+router.get('/apps/:id', async (req, res) => {
+
+    console.log("ID to search " + req.params.id);
+    let idApp = req.params.id;
+
+    pool.getConnection((err, conn) => {
+
+        if (!idApp) {
+            res
+                .status(400)
+                .send({
+                    status: "FAILED",
+                    data: { error: "Parameter ':idApp' can not be empty" },
+                });
+        }
+
+        try {
+            const query = `SELECT * FROM world.city WHERE city.ID = ?`;
+            conn.query(query, [idApp], (err, result) => {
+                conn.release();
+                if (err) throw err;
+                res.send({ status: "OK", data: result });
+            });
+        } catch (err) {
+
+            res
+                .status(err?.status || 500)
+                .send({ status: "FAILED", data: { error: err?.message || err } });
             res.end();
         }
 
@@ -28,10 +63,16 @@ router.get('/apps', async (req, res) => {
 
 router.post('/apps', async (req, res) => {
 
-    const Name = 'CalleLapas';
-    const CountryCode = 'BOL';
-    const District = 'Grunnon';
-    const Population = 45678;
+
+    // let Name = 'CalleLapas';
+    // let CountryCode = 'BOL';
+    // let District = 'Grunnon';
+    // let Population = 45678;
+
+    let Name = 'CalleLapas';
+    let CountryCode = 'BOL';
+    let District = 'Grunnon';
+    let Population = 45678;
 
     pool.getConnection((err, conn) => {
 
